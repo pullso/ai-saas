e9b508362c1229970089949b53355fe98ca67e42
pullso <pullso.code@gmail.com>
2023-11-22 21:31:34 +0300

    auth has been added

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,3 +1,10 @@
+import { UserButton } from '@clerk/nextjs'
+
 export default function DashboardPage() {
-	return <div>dashboard (protected)</div>
+	return (
+		<div>
+			<UserButton afterSignOutUrl='/' />
+			dashboard (protected)
+		</div>
+	)
 }
