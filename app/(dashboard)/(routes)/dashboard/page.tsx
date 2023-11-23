6a545d7e6af09a06eae966c46c725819d679ab43
pullso <pullso.code@gmail.com>
2023-11-23 23:00:05 +0300

    sidebar has been added update layout.tsx, page.tsx, page.tsx and tailwind.config.js

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,10 +1,3 @@
-import { UserButton } from '@clerk/nextjs'
-
 export default function DashboardPage() {
-	return (
-		<div>
-			<UserButton afterSignOutUrl='/' />
-			dashboard (protected)
-		</div>
-	)
+	return <div>dashboard (protected)</div>
 }
